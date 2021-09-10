6ad8d27b17914b9153af9741e4723e7b603188e8
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2021-09-10 13:18:01 +0200

    Add JSON & JavaScript Updates

--- js/app.js
+++ js/app.js
@@ -1,47 +1,15 @@
-
-let team = {
-    "members": [{
-        "Firstname": "Andreas",
-        "Lastname": "Knudsen",
-        "Age": 22,
-        "Height": 172,
-        "EyeColor": "Green"
-    },
-    {
-        "Firstname": "Jens",
-        "Lastname": "Jensen",
-        "Age": 87,
-        "Height": 140,
-        "EyeColor": "Grey"
-    },
-    {
-        "Firstname": "Kim",
-        "Lastname": "Larsen",
-        "Age": 45,
-        "Height": 191,
-        "EyeColor": "Blue"
-    },
-    {
-        "Firstname": "Troels",
-        "Lastname": "Hansen",
-        "Age": 33,
-        "Height": 180,
-        "EyeColor": "Brown"
-    }]
-}
-
 let list = document.querySelector("#person-data");
 
-
-team.members.forEach(Element => {
-    let firstname = document.createElement('li');
-    list.appendChild(firstname).innerHTML =
-    Element.Firstname + " " + Element.Lastname
-    + "<br>Alder: " + Element.Age + "<br>Højde: " + Element.Height
-    + "<br>Øjenfarve: " + Element.EyeColor;
-
-    
-    console.log(Element.Firstname + '\n' + Element.Lastname);
+fetch('./data.json')
+.then(response => response.json())
+.then((data) => {
+    data.members.forEach(Element => {
+        let Firstname = document.createElement('li');
+        Firstname.textContent = `${Element.Firstname}`
+        list.appendChild(Firstname);
+    });
 });
-    
+
+
+
 
